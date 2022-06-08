cf98775f5ba976ae31def2b9d0f71ed8339a49a5
Allen Mordukhaev <allenmor93@gmail.com>
2022-06-08 14:59:50 -0400

    commit tree 50a438042fa473e0251684ccfeef6f6ed0b15afb

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Write your code here!
+const main = document.getElementById('main');
+
+main.remove();
+
+
+const newHeader = document.createElement('h1');
+
+newHeader.setAttribute('id', 'victory');
+
+newHeader.innerHTML = 'Your Name is the champion'
